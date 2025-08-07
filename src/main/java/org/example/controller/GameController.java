280f78e0f389ded6723fde5a5153087bd317e04d
Kyle Morley <morleymkyle@gmail.com>
2025-08-07 15:23:32 +1000

    SRP - GameController, GameBoard, GameScreen and Tetromino Type all have single responsibilities
    OCP - We can now extend gameboard for things like scoring. We should consider adding interfaces for ISP too
    
    LSP - Not Applicable as we don't have inheritance at this stage
    
    DIP - We need interfaces here too
    
    We now have MVC structure and are positioned to implement a SOLID compliant application

---
+++ src/main/java/org/example/controller/GameController.java
@@ -0,0 +1,47 @@
+package org.example.controller;
+
+import javafx.animation.AnimationTimer;
+import javafx.scene.input.KeyCode;
+import org.example.model.GameBoard;
+
+public class GameController {
+    private static final long FALL_INTERVAL_NS = 500_000_000;
+    private long lastFallTime = 0;
+    private boolean paused = false;
+    private final GameBoard board;
+
+    public GameController(GameBoard board) {
+        this.board = board;
+    }
+
+    public void handleKey(KeyCode code) {
+        if (code == KeyCode.P) {
+            paused = !paused;
+            return;
+        }
+        if (paused) return;
+
+        switch (code) {
+            case LEFT -> board.move(-1, 0);
+            case RIGHT -> board.move(1, 0);
+            case DOWN -> board.move(0, 1);
+            case UP -> board.rotatePiece();
+        }
+    }
+
+    public void start(AnimationTimer timer) {
+        timer.start();
+    }
+
+    public void update(long now) {
+        if (paused) return;
+        if (lastFallTime == 0 || now - lastFallTime > FALL_INTERVAL_NS) {
+            board.tick();
+            lastFallTime = now;
+        }
+    }
+
+    public boolean isPaused() {
+        return paused;
+    }
+}
